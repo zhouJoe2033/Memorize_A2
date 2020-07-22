739dce224fdade24f2cdae2d2ecaf5e94ddbbd78
zhouJoe2033 <49823919+zhouJoe2033@users.noreply.github.com>
2020-07-22 16:51:49 -0400

    Initial Commit

---
+++ Assignment_two/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Assignment_two
+//
+//  Created by Joe on 2020-07-22.
+//  Copyright © 2020 Joe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
